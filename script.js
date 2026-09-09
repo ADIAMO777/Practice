cd2b199254f14196fc683844d23c116c37d2ad8c
masterminders-maker <mastermindtechacademy@gmail.com>
2026-09-09 01:38:04 +0100

    update

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-let output = document.createElement("div");
+// let output = document.createElement("div");
 
-for (let i = 0; i < 10; i++) {
-    output.innerHTML += "I love 😊 you! Browse our selection of organic produce<br>";
+// for (let i = 0; i < 10; i++) {
+//     output.innerHTML += "I love 😊 you! Browse our selection of organic produce<br>";
+// }
+
+
+// document.body.appendChild(output);
+
+// var heading = document.querySelector("main h3");
+
+// let input = document.createElement("div");
+
+// for (let i = 0; i < 10; i++) {
+//     output.innerHTML += `${heading.innerHTML}<br>\n`;
+// }
+
+// document.body.appendChild(output);
+
+
+let name = 'Bankole-Adiamo';
+let age = 20;
+let tutor = 'John Doe';
+
+console.log(`hello My name is ${name} and I am ${age} years old.`);
+console.log(`My tutor is ${tutor}.`);
+
+function greetUser(userName) {
+    console.log(`Hello, ${userName}! Welcome to our website.`);
 }
+greetUser(tutor);
 
-document.body.appendChild(output);
-
-var heading = document.querySelector("main h3");
-let input = document.createElement("div");
-
-for (let i = 0; i < 10; i++) {
-    output.innerHTML += `${heading.innerHTML}<br>\n`;
-}
-
-document.body.appendChild(output); 
+console.log(`hello ${tutor} My name is ${name} and I am ${age} years old & I am a student of ${tutor}, Welcome to our website.`);
